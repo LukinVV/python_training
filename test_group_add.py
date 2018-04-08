ce365a88d428495c57c4f707b1e89510d91e5ab7
Lukin <LukinVV@inbox.ru>
2018-04-08 14:40:00 +0300

    создаем методы

--- test_group_add.py
+++ test_group_add.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-# from selenium.webdriver.firefox.webdriver import WebDriver так и не смог заупстить в Firefox, бразер открывается
-#  но ни чего не происходит
-# from selenium.webdriver.chrome.webdriver import WebDriver
 
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 
 chrome=webdriver.Chrome()
+#firefox=webdriver.Firefox(capabilities={"marionette": False}) так и не смог заупстить в Firefox, бразер открывается
+#  но ни чего не происходит
 
 def is_alert_present(wd):
     try:
@@ -20,11 +18,11 @@ class test_group_add(unittest.TestCase):
     def setUp(self):
         self.wd = chrome
         self.wd.implicitly_wait(60)
-    
-    def test_test_group_add(self):
-        success = True
-        wd = self.wd
-        wd.get("http://localhost/addressbook/") # переход к тестовой странице
+
+    def open_start_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -36,24 +34,44 @@ class test_group_add(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
+
+    def return_to_page_groups(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        # логаут
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_new_group(self, wd):
+        # создание новой группы
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asda") # название имени группы
+        wd.find_element_by_name("group_name").send_keys("asda")  # название имени группы
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("asdasd") # название header
+        wd.find_element_by_name("group_header").send_keys("asdasd")  # название header
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("asdasd") # название footer
+        wd.find_element_by_name("group_footer").send_keys("asdasd")  # название footer
+        # завершили создание новой группы
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+
+    def open_page_groups(self, wd):
+        # открытие страницы groups
+        wd.find_element_by_link_text("groups").click()
+
     def tearDown(self):
         self.wd.quit()
+
+    def test_test_group_add(self):
+        wd = self.wd
+        self.open_start_page(wd)
+        self.login(wd)
+        self.open_page_groups(wd)
+        self.create_new_group(wd)
+        self.return_to_page_groups(wd)
+        self.logout(wd)
 
 if __name__ == '__main__':
     unittest.main()
