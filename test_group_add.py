f963bce031df3b96eeeeaa44709d945d5849e907
Lukin <LukinVV@inbox.ru>
2018-04-08 19:56:34 +0300

    Задание №2: Создать вспомогательные методы и классы

--- test_group_add.py
+++ test_group_add.py
@@ -2,9 +2,9 @@
 
 from selenium import webdriver
 import time, unittest
+from group import Group
 
-chrome=webdriver.Chrome()
-#firefox=webdriver.Firefox(capabilities={"marionette": False}) так и не смог заупстить в Firefox, бразер открывается
+#firefox=webdriver.Firefox(capabilities={"marionette": False}) так и не смог заупстить в Firefox, браузер открывается
 #  но ни чего не происходит
 
 def is_alert_present(wd):
@@ -16,44 +16,45 @@ def is_alert_present(wd):
 
 class test_group_add(unittest.TestCase):
     def setUp(self):
+        chrome = webdriver.Chrome()
         self.wd = chrome
         self.wd.implicitly_wait(60)
 
     def open_start_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, user_name, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(user_name)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def return_to_page_groups(self, wd):
         wd.find_element_by_link_text("group page").click()
 
     def logout(self, wd):
-        # логаут
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_group(self, wd):
-        # создание новой группы
+    def create_new_group(self, wd, group):
+        # создаем группу
         wd.find_element_by_name("new").click()
+        # вводим данные
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asda")  # название имени группы
+        wd.find_element_by_name("group_name").send_keys(group.name)  # название имени группы
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("asdasd")  # название header
+        wd.find_element_by_name("group_header").send_keys(group.header)  # название header
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("asdasd")  # название footer
+        wd.find_element_by_name("group_footer").send_keys(group.footer)  # название footer
         # завершили создание новой группы
         wd.find_element_by_name("submit").click()
 
@@ -61,17 +62,26 @@ class test_group_add(unittest.TestCase):
         # открытие страницы groups
         wd.find_element_by_link_text("groups").click()
 
-    def tearDown(self):
-        self.wd.quit()
-
-    def test_test_group_add(self):
+    def test_empty_group_add(self):
         wd = self.wd
         self.open_start_page(wd)
-        self.login(wd)
+        self.login(wd, user_name="admin", password="secret")
         self.open_page_groups(wd)
-        self.create_new_group(wd)
+        self.create_new_group(wd, Group(name="", header="", footer=""))
         self.return_to_page_groups(wd)
         self.logout(wd)
+
+    def test_group_add(self):
+        wd = self.wd
+        self.open_start_page(wd)
+        self.login(wd, user_name="admin", password="secret")
+        self.open_page_groups(wd)
+        self.create_new_group(wd, Group(name="NAME", header="HEADER", footer="FOOTER"))
+        self.return_to_page_groups(wd)
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
 
 if __name__ == '__main__':
     unittest.main()
