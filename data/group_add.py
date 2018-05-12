55c22ad837a0ee5bfb83d8ad9fc1a2a13130a551
gramb <Lukinvv@inbox.ru>
2018-05-12 10:44:43 +0300

    Отделение тестовых данных от сценариев для добавления групп и контактов

---
+++ data/group_add.py
@@ -0,0 +1,20 @@
+import random
+import string
+from model.group import Group
+
+
+
+
+def random_string(prefix, max_len):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
+
+#testdata
+# group = [Group(name="", header="", footer="")]+[Group(name=random_string('name', 10),
+#                    header=random_string('header', 20), footer=random_string('footer', 20)) for i in range(5)]
+
+group_new = [Group(name=name, header=header, footer=footer)
+             for name in ["", random_string('name', 10)]
+             for header in ["", random_string('header', 10)]
+             for footer in ["", random_string('footer', 10)]
+             ]
--- test/test_group_add.py
+++ test/test_group_add.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-import random
-import string
+from data.group_add import group_new
 import pytest
 from model.group import Group
 
-
-
-
-def random_string(prefix, max_len):
-    symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
-
-#testdata
-# group = [Group(name="", header="", footer="")]+[Group(name=random_string('name', 10),
-#                    header=random_string('header', 20), footer=random_string('footer', 20)) for i in range(5)]
-
-group_new = [Group(name=name, header=header, footer=footer)
-             for name in ["", random_string('name', 10)]
-             for header in ["", random_string('header', 10)]
-             for footer in ["", random_string('footer', 10)]
-             ]
 
 @pytest.mark.parametrize("group", group_new, ids=[repr(x) for x in group_new])
 def test_group_add(app, group):
