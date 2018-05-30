17dc5e8b782d2085d4a0ad2a0268c7d8aecdb60f
gramb <Lukinvv@inbox.ru>
2018-05-30 21:44:10 +0300

    Доработка дополнительные проверки для теста по добавлению/удалению контакта в группы

--- test/test_contact_in_groups.py
+++ test/test_contact_in_groups.py
@@ -9,8 +9,14 @@ def test_add_contact_to_group(app, orm):
     if len(orm.get_contacts_not_in_groups(group)) == 0:
         app.contact.create_new(Contact(firstname="New"))
     contact = random.choice(orm.get_contacts_not_in_groups(group))
+    if contact in orm.get_contacts_in_groups(group):
+        app.contact.delete_contact_from_group(contact.id, group.id)
     app.contact.add_to_group(contact.id, group.id)
     assert contact in orm.get_contacts_in_groups(group)
+    assert contact not in orm.get_contacts_not_in_groups(group)
+    orm.get_contacts_not_in_groups(group).append(contact)
+    sorted(orm.get_contacts_not_in_groups(group), key=Contact.id_or_max) == sorted(orm.get_contacts_in_groups(group),
+                                                                                   key=Contact.id_or_max)
 
 
 
@@ -25,5 +31,11 @@ def test_remove_contact_from_group(app, orm):
         app.contact.add_to_group(contact.id, group.id)
     else:
         contact = random.choice(orm.get_contacts_in_groups(group))
+    if contact not in orm.get_contacts_in_groups(group):
+        app.contact.add_to_group(contact.id, group.id)
     app.contact.remove_from_group(contact.id, group.id)
     assert contact in orm.get_contacts_not_in_groups(group)
+    assert contact not in orm.get_contacts_in_groups(group)
+    orm.get_contacts_not_in_groups(group).remove(contact)
+    sorted(orm.get_contacts_not_in_groups(group), key=Contact.id_or_max) == sorted(orm.get_contacts_in_groups(group),
+                                                                                   key=Contact.id_or_max)
